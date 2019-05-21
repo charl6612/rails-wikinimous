167a6bd204fbf857bf7214111d0407035cabdcb7
charles noppe <noppe.charles@gmail.com>
2019-05-21 16:33:59 +0200

    ex

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do 
+  article = Article.new(title: Faker::Superhero.name, content: Faker::ChuckNorris.fact)
+  article.save
+end
